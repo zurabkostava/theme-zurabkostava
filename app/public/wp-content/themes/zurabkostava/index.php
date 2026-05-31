f1af9b796ae90cf6d508ac64fa77f79f8c7461ec
zurabkostava <zurabkostava1@gmail.com>
2026-05-31 19:09:50 +0400

    test header

---
+++ app/public/wp-content/themes/zurabkostava/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title>Zurab Kostava - Test</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <?php wp_head(); ?>
+</head>
+<body>
+<header class="test-header">
+    <h1>Zurab Kostava</h1>
+    <p>CI/CD ტესტირება წარმატებულია</p>
+</header>
+<?php wp_footer(); ?>
+</body>
+</html>
