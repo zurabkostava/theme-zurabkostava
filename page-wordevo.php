e561885fff0d63af8314374139bd286d63a19dd4
zurabkostava <zurabkostava1@gmail.com>
2026-06-18 01:14:47 +0400

    Fix CSS loading via wp_enqueue_style

--- page-wordevo.php
+++ page-wordevo.php
@@ -10,6 +10,9 @@ add_action('wp_enqueue_scripts', function() {
     wp_dequeue_script('zk-app');
     wp_dequeue_style('wp-block-library');
     wp_dequeue_style('global-styles');
+    
+    // Enqueue WordEvo App Styles
+    wp_enqueue_style('wordevo-app-style', get_template_directory_uri() . '/WordEvo/style.css', array(), '12');
 }, 999);
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
@@ -29,8 +32,6 @@ remove_action('wp_footer', 'zk_mobile_bottom_nav'); // Remove Mobile Bottom Nav 
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@700;900&display=swap" rel="stylesheet">
     <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/WordEvo/manifest.json">
     <meta name="theme-color" content="#ffffff">
-
-    <link href="<?php echo get_template_directory_uri(); ?>/WordEvo/style.css?v=11" rel="stylesheet"/>
 
     <script>
         window.WORDEVO_ASSET_PATH = '<?php echo get_template_directory_uri(); ?>/WordEvo';
