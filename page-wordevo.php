0282be444ce5affec07f59119cf8b2bc6a8d4d79
johnkostava <kostavaj@gmail.com>
2026-06-19 23:07:15 +0400

    UI: remove tag icon from mobile toolbar and fix stretched notification button

--- page-wordevo.php
+++ page-wordevo.php
@@ -131,9 +131,6 @@ remove_action('wp_footer', 'zk_mobile_bottom_nav'); // Remove Mobile Bottom Nav 
     </div>
     <div class="card-toolbar" id="cardToolbar">
             <div class="toolbar-left">
-                <button id="toggleSidebarBtn" title="თეგები">
-                    <i class="fas fa-tags"></i>
-                </button>
                 <button class="toolbar-btn" id="statsBtn" title="სტატისტიკა">
                     <i class="fas fa-chart-pie"></i>
                 </button>
